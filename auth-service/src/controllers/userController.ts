92e5baed3a3f8890b8620504506f592d33a4d1f3
kavidu717 <dushmanthakavidu143@gmail.com>
2026-08-04 00:32:44 +0530

    feat: implement user registration and authentication routing for auth-service

--- auth-service/src/controllers/userController.ts
+++ auth-service/src/controllers/userController.ts
@@ -1,4 +1,8 @@
+import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
+import User from "../models/User";
+import { Request, Response } from "express";
+import { sendOtpEmail } from "../services/emailService";
 
 
 
@@ -29,3 +33,43 @@ const generateRefreshToken = (id: string): string => {
         )
 }
 
+// user register function
+export const registerUser = asyncHandler(async (req: Request, res: Response) => {
+    const { firstName, lastName, email, password } = req.body
+
+    // check the email is already exist 
+    const userExits = await User.findOne({ email });
+    if (userExits) {
+        throw new Error("Email is already exist");
+    }
+
+    const otpCode = generateOtp();
+    const otpExpires = new Date(Date.now() + 15 * 60000)
+
+    const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        password,
+        otpCode,
+        otpExpires,
+
+    })
+
+    if (user) {
+        await sendOtpEmail(user.email, otpCode)
+
+        res.status(201).json({
+            message: "User registered successfully.Please verify your email",
+            email: user.email,
+        })
+
+
+    } else {
+        res.status(400).json({
+            message: "invalid user data received",
+        })
+    }
+
+
+})
--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,6 +1,7 @@
 
 import express, { Application, Request, Response } from "express";
 import connectDB from "./config/db";
+import userRotes from "./routes/userRoutes"
 
 
 
@@ -12,6 +13,8 @@ const app: Application = express();
 app.use(express.json());
 
 connectDB();
+
+app.use('api/auth', userRotes)
 
 app.get("/health", (req: Request, res: Response) => {
     res.json({
