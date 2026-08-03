95c87fb4be5d6a3b5531c0fc868e6d7b0a35d5b2
kavidu717 <dushmanthakavidu143@gmail.com>
2026-08-04 00:09:09 +0530

    feat: implement OTP generation and JWT access/refresh token signing utilities in userController

--- auth-service/src/controllers/userController.ts
+++ auth-service/src/controllers/userController.ts
@@ -1,4 +1,31 @@
+import jwt from "jsonwebtoken";
 
 
 
 // generate the random 6 digit otp
+const generateOtp = () => {
+    return Math.floor(100000 + Math.random() * 900000).toString();
+};
+
+// generate the access token
+const generateAccessToken = (id: string, role: string): string => {
+    return jwt.sign
+        (
+            { id, role }, process.env.JWT_SECRET as string, {
+            expiresIn: '15m',
+        }
+        );
+};
+
+//generate Refresh token
+const generateRefreshToken = (id: string): string => {
+    return jwt.sign
+        (
+            { id },
+            process.env.JWT_SECRET as string,
+            {
+                expiresIn: "7d", // 1 week
+            }
+        )
+}
+
