45c78b1860dbe840821c23bc2e8d13486b37ef36
kavidu717 <dushmanthakavidu143@gmail.com>
2026-08-08 22:09:32 +0530

    feat: add RabbitMQ connection utility and channel initialization module

---
+++ monitoring-engine/src/utils/rabbitmq.ts
@@ -0,0 +1,32 @@
+import ampq, { Connection, Channel, ChannelModel } from 'amqplib'
+
+
+let connection: ChannelModel
+let channel: Channel;
+
+
+export const connectRabbitMQ = async () => {
+    try {
+        const ampqServer = process.env.RABBITMQ_URL || `amqp://localhost:5672`
+        connection = await ampq.connect(ampqServer)
+        channel = await connection.createChannel()
+
+        await channel.assertQueue('health_check', { durable: true })
+        console.log("RabbitMQ connected")
+
+
+
+
+    }
+    catch (error) {
+        console.log("Error in connecting to RabbitMQ", error)
+        process.exit(1);
+    }
+}
+
+export const getChannel = () => {
+    if (!channel) {
+        throw new Error('RabbitMQ channel not initialized!');
+    }
+    return channel;
+};
