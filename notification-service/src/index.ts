b26c9957bd7d1ef78d0d0e70090a01cc3298ce7a
kavidu717 <dushmanthakavidu143@gmail.com>
2026-08-18 18:29:17 +0530

    feat: add RabbitMQ health check consumer

--- notification-service/src/index.ts
+++ notification-service/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
+import amqp from 'amqplib';
 
 
 dotenv.config();
@@ -11,7 +12,47 @@ const PORT = process.env.PORT || 5004;
 
 connectDB();
 
-app.listen(PORT, () => {
-    console.log(`Notification service running on port ${PORT}`);
-});
 
+const startConsumer = async () => {
+    try {
+
+        const ampqServer = process.env.RABBITMQ_URL || 'amqp://localhost:5672';
+        const connection = await amqp.connect(ampqServer)
+        const channel = await connection.createChannel()
+
+        const queue = 'health_check'
+        await channel.assertQueue(queue, { durable: true })
+
+        console.log(`RabbitMQ Connected. Waiting for messages in "${queue}"...`);
+
+        channel.consume(queue, (msg) => {
+            if (msg !== null) {
+                const data = JSON.parse(msg.content.toString());
+
+                const actualLatency = data.latency || data.time;
+                console.log(`[+] Received Data -> URL: ${data.url} | Status: ${data.status} | Latency: ${actualLatency}ms`);
+                channel.ack(msg);
+
+            }
+        })
+
+
+
+    } catch (error) {
+        console.error('error conneting to rabbitmq', error);
+    }
+
+
+
+}
+
+const startServer = async () => {
+    await connectDB();
+    await startConsumer();
+
+    app.listen(PORT, () => {
+        console.log(`Notification & Analytics Service is running on port ${PORT}`);
+    });
+};
+
+startServer();
